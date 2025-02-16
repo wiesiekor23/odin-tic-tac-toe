5d5bb3d5e8d43e188b8f3691ecde035fc5d8d11e
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2025-02-16 16:38:32 +0000

    Add function to control game flow(console)
    - Function to determine winner along with winning
    conditions added
    - Play function added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,7 @@
 function Players() {
-  
-  
-  let players = [{playerName: "name", mark: "X"}, {playerName: "name", mark: "O"}];
-  players[0].name = "Mark";
+
+
+  let players = [{ playerName: "name", mark: "X" }, { playerName: "name", mark: "O" }];
 
   function getPlayers() {
     return players;
@@ -12,12 +11,9 @@ function Players() {
     players = [players[1], players[0]];
     return players;
   }
-  changePlayers();
-  console.log(players);
-  return {getPlayers};
-}
 
-Players();
+  return { getPlayers, changePlayers };
+}
 
 
 
@@ -35,16 +31,64 @@ function GameBoard() {
   function changeBoardItem(newItem, index) {
     return board.splice(index, 1, newItem);
   }
-
-  return {getBoard, changeBoardItem};
+  
+  return { getBoard, changeBoardItem };
 };
 
-const board = GameBoard();
-const players = Players();
-board.changeBoardItem(players.getPlayers()[1].playerName, 2);
 
-console.log(board.getBoard()[2]);
-/* function checkWinningCondition(array) {
-  if()
-} */
+function gameFlow() {
+  const players = Players();
+  const board = GameBoard();
+  
+players.getPlayers()[0].playerName = "Steve";
+players.getPlayers()[1].playerName = "Stacey";
+  
+  function play() {
+    const activePlayer = players.getPlayers()[0].playerName;
+    const playerMark = players.getPlayers()[0].mark;
+    let indexInput = prompt(`${activePlayer}'s turn!`);
+    board.changeBoardItem(playerMark, indexInput);
+    
+    if (checkWinningConditions() == "Win") {
+      console.log(`${activePlayer} won`);
+    } else if (checkWinningConditions() == "Draw") {
+      console.log("It's a draw");
+    } else {
+      players.changePlayers();
+      play();
+    }
+  }
+  
+  function checkWinningConditions() {
+    const boardCheck = board.getBoard();
+    let returnedValue;
+    const winningConditions = [
+      [boardCheck[0], boardCheck[1], boardCheck[2]],
+      [boardCheck[3], boardCheck[4], boardCheck[5]],
+      [boardCheck[6], boardCheck[7], boardCheck[8]],
+      [boardCheck[0], boardCheck[3], boardCheck[6]],
+      [boardCheck[1], boardCheck[4], boardCheck[7]],
+      [boardCheck[2], boardCheck[5], boardCheck[8]],
+      [boardCheck[0], boardCheck[4], boardCheck[8]],
+      [boardCheck[2], boardCheck[4], boardCheck[6]],
+    ];
+    
+    for (let condition of winningConditions) {
+      if (condition.every(cell => cell === "X") || condition.every(cell => cell === "O")) {
 
+        returnedValue = "Win";
+        return returnedValue;
+      };
+    };
+    
+    if (boardCheck.every(cell => cell !== "")) {
+      returnedValue = "Draw";
+    }
+    
+    return returnedValue;
+  };
+  
+  play();
+};
+
+gameFlow();
