a7cd2f494875847fce758bfdd66f25d08662ed66
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2025-02-23 19:20:11 +0000

    Create functionality for adding player names

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -22,32 +22,37 @@ function GameBoard() {
   for (let i = 0; i < 9; i++) {
     board.push("");
   }
-  
+
   function getBoard() {
     return board;
   }
-  
+
   function updateBoardItem(newItem, index) {
     board[index] = newItem;
     return board[index];
   }
-  
+
   return { getBoard, updateBoardItem };
-};  //return { createBoardDisplay, updateDisplay };
+};
 
 
 function gameFlow() {
   const players = Players();
   const board = GameBoard();
-  
-  players.getPlayers()[0].playerName = "Steve";
-  players.getPlayers()[1].playerName = "Stacey";
-  
+
+  function addPlayerOne(playerOne) {
+    players.getPlayers()[0].playerName = playerOne;
+  }
+
+  function addPlayerTwo(playerTwo) {
+    players.getPlayers()[1].playerName = playerTwo;
+  }
+
   function play(index) {
     const activePlayer = players.getPlayers()[0].playerName;
     const playerMark = players.getPlayers()[0].mark;
     board.updateBoardItem(playerMark, index);
-    
+
     if (checkWinningConditions() == "Win") {
       console.log(`${activePlayer} won`);
       return;
@@ -59,7 +64,7 @@ function gameFlow() {
     players.swapPlayers();
     console.log(board.getBoard());
   };
-  
+
   function checkWinningConditions() {
     const boardCheck = board.getBoard();
     let returnedValue = "";
@@ -74,52 +79,63 @@ function gameFlow() {
       [boardCheck[0], boardCheck[4], boardCheck[8]],
       [boardCheck[2], boardCheck[4], boardCheck[6]],
     ];
-    
+
     for (let condition of winningConditions) {
       if (condition.every(cell => cell === "X") || condition.every(cell => cell === "O")) {
         returnedValue = "Win";
         return returnedValue;
       };
     };
-    
+
     if (boardCheck.every(cell => cell !== "")) {
       returnedValue = "Draw";
     }
     return returnedValue;
   };
-  
-  return { play, board };
+
+  return { play, board, addPlayerOne, addPlayerTwo };
 };
 
 function displayController() {
   const game = gameFlow();
   const displayBoard = document.querySelector(".game-container");
   const board = game.board.getBoard();
-  
+
   function createBoardDisplay(board) {
     displayBoard.innerHTML = "";
-    
+
     board.forEach((element, index) => {
       const gameButton = document.createElement("button");
       gameButton.classList.add(`cell`, `${index}`);
       displayBoard.appendChild(gameButton);
     });
   };
-  
+
   function updateDisplay() {
     const cells = document.querySelectorAll(`.cell`);
     cells.forEach((cell, index) => {
       cell.addEventListener("click", () => {
-        console.log(index);
         game.play(index);
         cell.textContent = board[index];
-        console.log(board[index]);
-      }, {once: true});
+      }, { once: true });
     });
   };
 
+  function addNames() {
+    document.querySelector(".submit-button-one").addEventListener("click", () => {
+      const inputOne = document.querySelector("#input-field-one").value;
+      game.addPlayerOne(inputOne);
+    });
+    
+    document.querySelector(".submit-button-two").addEventListener("click", () => {
+      const inputTwo = document.querySelector("#input-field-two").value;
+      game.addPlayerTwo(inputTwo);
+    });
+  };
+  
   createBoardDisplay(board);
   updateDisplay();
+  addNames();
 };
 
 displayController();
